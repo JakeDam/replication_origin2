37c5df866228bb3bf8b42c0622d2a9d5eaf80d86
JakeDam <robert.dam2@gmail.com>
2021-04-13 20:00:51 -0400

    week complete

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ Python script that contains tools for analyzing the repilication origin of genom
 - hamming_dist: Calculates the Hamming Distance between two nucleotide sequences
 - approximate_match: Determines the location of approximate (within a certian number of bases) matches of a pattern to a genome
 - approximate_pattern_count: Counts the number of approximate matches of a pattern to a genome
+-neighbors: Generates the d-neighborhood (set of all k-mers whose hamming distance from the pattern does not exceed d)
+ for a given DNA sequence
+ -immidiate_neighbors: Generates the d=1 neighborhood for a given DNA sequence
+ -max_map: Returns the maximum value from a dictionary 
+ -freq_words_with_mismatches: Returns the most frequent k-mer mismaches to a pattern with mismatches less than or equal to a given value, d
+ -reverse_complement: Takes in a DNA sequence and returns its reverse complement
+ -freq_mismatch_rev_comp: Returns the most frequent k-mer mismatches including reverse complements to a pattern with mismatches less than or equal to a given value, d
--- script.py
+++ script.py
@@ -64,7 +64,7 @@ def approximate_pattern_count(pattern, genome, d):
 # Generates the d-neighborhood (set of all k-mers whose hamming distance from the pattern does not exceed d)
 def neighbors(pattern, d):
     if d == 0:
-        return set((pattern))
+        return set([pattern])
     if len(pattern) == 1:
         return set(('A', 'T', 'C', 'G'))
     neighborhood = set()
@@ -88,38 +88,66 @@ def immediate_neighbors(pattern):
                 neighborhood.add(neighbor)
     return neighborhood
 
-# Iterative version of neighbors
-def iter_neighbors(pattern, d):
-    neighborhood = set((pattern))
-    for j in range(1, d + 1):
-        for text in neighborhood:
-            temp_set = set()
-            temp_set.add(immediate_neighbors(text))
-        neighborhood.add(temp_set)
-    return neighborhood
-
 # Returns the largest value from a dictionary 
 def max_map(dict):
     return max(dict.values())
 
-# Returns all of the approximate matches of a pattern to a given genome
+# Returns the most frequent k-mer approximate matches of a pattern to a given genome
 def freq_words_with_mismatches(genome, k, d):
     patterns = []
     freq_dict = {}
     length = len(genome)
-    for i in range(0, length - k + 1):
-        pattern = genome[i:k]
-        neighborhood = iter_neighbors(pattern, d)
+    for i in range(length - k + 1):
+        pattern = genome[i:i + k]
+        neighborhood = neighbors(pattern, d)
         for neighbor in neighborhood:
             if neighbor not in freq_dict:
                 freq_dict[neighbor] = 1
             else:
                 freq_dict[neighbor] += 1
     m = max_map(freq_dict)
-    for key in freq_dict:
-        if freq_dict[key] == m:
-            patterns.append(key)
+    for pattern in freq_dict:
+        if freq_dict[pattern] == m:
+            patterns.append(pattern)
     return patterns
 
+# Takes in a DNA sequence and returns its reverse complement 
+def reverse_complement(dna):
+    complement = ''
+    for base in dna:
+        if (base == 'A' or base == 'a'):
+            complement += 'T'
+        elif (base == 'T' or base == 't'):
+            complement += 'A'
+        elif (base == 'C' or base == 'c'):
+            complement += 'G'
+        else:
+            complement += 'C'
+    rev_comp = complement[::-1]
+    return rev_comp
 
-print(iter_neighbors('ACGTTGCATGTCGCATGATGCATGAGAGCT', 1))
+# Returns the most frequent k-mer approximate matches and reverse complements of a pattern to a given genome
+def freq_mismatch_rev_comp(genome, k, d):
+    patterns = []
+    freq_dict = {}
+    length = len(genome)
+    for i in range(length - k + 1):
+        pattern = genome[i:i + k]
+        neighborhood = neighbors(pattern, d)
+        for neighbor in neighborhood:
+            if neighbor not in freq_dict:
+                freq_dict[neighbor] = 1
+            else:
+                freq_dict[neighbor] += 1
+            rev_comp = reverse_complement(neighbor)
+            if rev_comp not in freq_dict:
+                freq_dict[rev_comp] = 1
+            else:
+                freq_dict[rev_comp] += 1
+    m = max_map(freq_dict)
+    for pattern in freq_dict:
+        if freq_dict[pattern] == m:
+            patterns.append(pattern)
+    return patterns
+
+print(len(freq_words_with_mismatches('TGCAT', 5, 2)))
